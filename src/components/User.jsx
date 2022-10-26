601b00794c0fb68db44ac51be414556c8064d5da
Ali <ali.ozgur@example.com>
2022-10-27 01:26:04 +0300

    created style

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -17,10 +17,27 @@ const User = () => {
         getUsers();
       }, []);
 
-console.log(user)
+console.log(user);
+// api den çekeçeğim bilgileri decsturcturing yöntemiyle parçalıyorum.
+const { name, dob, email, picture, location } = user;
 
   return (
-    <div>User</div>
+    
+    <div className= "container text-center w-75">
+        <div className= " container bg-warning rounded-circle mt-5 p-5 text center w-50">      
+                <h2 className="mt-3 text-danger fw-bold">
+        {/* Optional Chaining */}
+        {name?.first} {name?.last}
+      </h2>
+      <img className=" p-3 rounded-circle w-50" src={picture?.large} alt="img" />
+      <p>{email}</p>
+      <p className="fw-bolder">{location?.country}</p>
+      <h6>{new Date(dob?.date).toLocaleDateString("tr-TR")}</h6>
+      <button className="btn btn-success my-3" onClick={getUsers}>
+        Get User
+      </button>
+      </div> 
+    </div>
   )
 }
 
