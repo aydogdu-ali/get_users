faf4dad924e5cb5e0c13dbbf7ad4ffd40779ff7d
Ali <ali.ozgur@example.com>
2022-10-27 00:55:45 +0300

    fetched data and created function

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from "react";
 
 const User = () => {
+    const [user, setUser] = useState("");
+
+
+    // veriyi fetch yöntemi ile çekiyorum
+    const getUsers = () => {
+        fetch("https://randomuser.me/api")
+          .then((res) => res.json())
+          .then((data) => setUser(data.results[0]));
+      };
+    
+      // veriyi Dom' a çağırmak için useEffect kullanıyorum
+      useEffect(() => {
+        getUsers();
+      }, []);
+
+console.log(user)
+
   return (
     <div>User</div>
   )
